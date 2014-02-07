0ff20c255c9aefc5dfc65471b3ee72fd67097467
Håvard Magne Fagervoll <fagervoll@gmail.com>
2014-02-07 13:38:43 +0100

    Gjør noen endringer

--- kode.m
+++ kode.m
@@ -1,6 +1,6 @@
 function kode()
     %telefon_nr = input('Vennligst skriv inn dit telefonnummer: ');
-    telefon_nr = '48123012';
+    telefon_nr = '4812sd3k012';
     lag_toner(telefon_nr);
     %spill_av(toner);
     
@@ -28,22 +28,27 @@ function lag_toner(tall)
     for i=1:8,
         disp(i)
         siffer = tall(i);
-        if siffer == '#'
-            siffer = '12';
-        elseif siffer == '*'
-            siffer = '10';
-        elseif siffer == '0'
-            siffer = '11';
-        end
-        x1 = cos(total(1,str2num(siffer))*2*pi*t);
-        x2 = cos(total(2,str2num(siffer))*2*pi*t);
-        y = x1+x2;
-        sound(y, Fs);
-        pause(0.7);
         
-    else
+        if siffer=='#'||siffer=='*'||siffer=='0'||siffer=='1'||siffer=='2'||siffer=='3'||siffer=='4'||siffer=='5'||siffer=='6'||siffer=='7'||siffer=='8'||siffer=='9'
+
+            if siffer == '#'
+                siffer = '12';
+            elseif siffer == '*'
+                siffer = '10';
+            elseif siffer == '0'
+                siffer = '11';
+            end
+            x1 = cos(total(1,str2num(siffer))*2*pi*t);
+            x2 = cos(total(2,str2num(siffer))*2*pi*t);
+            y = x1+x2;
+            sound(y, Fs);
+            pause(0.7);
+        
+        else
             disp('Det finnes en feil i nummeret du oppgav. Prøv igjen.');
-            kode()
+            %kode()
+            
+        end
     end
     
     

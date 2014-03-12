f0c4778a8fdab1ea4b90e64ee893704b97bb7fec
Sverre Turter Sandvold <Sverre@Sverres-MacBook-Pro.local>
2014-03-12 12:19:53 +0100

    Kommentering

--- main_part2.m
+++ main_part2.m
@@ -11,54 +11,54 @@ function main()
 end
 
 
-function filteret = lagFiltre()
-    filteret = [];
-    frekvenser = [697 770 852 941 1209 1336 1477];
-    Fs = 8000;
-    bredde = 300;
-    n = 0:bredde-1;
+function filteret = lagFiltre() %Funksjon som oppretter filteret
+    filteret = []; %Oppretter en matrise til filteret
+    frekvenser = [697 770 852 941 1209 1336 1477]; %Frekvensene til telefonen
+    Fs = 8000; %Samplingsfrekvensen
+    bredde = 300; %Bredden på filteret/Antall punkter
+    n = 0:bredde-1; %Tallrekke fra 0 til bredde-1
     
-    for i=1:length(frekvenser)
-        w0 = 2*pi*frekvenser(i)/Fs;
-        am = 2*cos(w0*n)/bredde;
-        
-        filteret = [filteret; am];
+    for i=1:length(frekvenser) %For-løkke som går gjennom alle frekvensene
+        w0 = 2*pi*frekvenser(i)/Fs; %Vinkelfrekvens
+        am = 2*cos(w0*n)/bredde; %Frekvenverdier til systemet/Signal vi ønsker å slippe gjennom BP-filteret
+    
+        filteret = [filteret; am]; %Legger frekvensverdier til i filteret
   
         %Plotter båndfiltrene!
-        [h w] = freqz(am, 1, bredde);
-        d = w*Fs/(2*pi);
-        plot(d, abs(h));
-        axis ([0 2000 0 1]);
-        hold on
+        [h w] = freqz(am, 1, bredde); %Beregner frekvensresponsen
+        d = w*Fs/(2*pi); % 
+        plot(d, abs(h)); %Plotter BP-filterer
+        axis ([0 2000 0 1]); %Setter aksene
+        hold on %Legger alle plottene i samme figur
     end
 end
 
-function tallene = finn_tall(toner)
-    Fs = 8000;
-    sum = 0;
-    bredde = 300;
-    frekvenser = [697 770 852 941 1209 1336 1477];
-    toneLengde = Fs*0.2;
-    pauseLengde = Fs*0.05;
-    antallTall = round(length(toner)/(toneLengde + pauseLengde));
-    filter2 = lagFiltre();
-    tallene = [];
+function tallene = finn_tall(toner) %Funksjon som finner hvilke tall som er trykket utifra frekvensene
+    Fs = 8000; %Samplingsfrekvens
+    sum = 0; %Sum av frekvenser
+    bredde = 300; %Bredden på filteret/Antall punkter
+    frekvenser = [697 770 852 941 1209 1336 1477];%Frekvensene
+    toneLengde = Fs*0.2; %Hvor lang hver tone er
+    pauseLengde = Fs*0.05; %Hvor lang hver pause er
+    antallTall = round(length(toner)/(toneLengde + pauseLengde)); %Finner antall tall som er trykker
+    filter2 = lagFiltre(); %Henter filtrene fra lagFiltre() som filter2
+    tallene = []; %Tom matrise som vi ende opp med tallene som er trykket
     
-    for s=0:antallTall-1
-        resultat = [];
-        start = round((s*(toneLengde + pauseLengde)) + 1);
-        slutt = round((s*(toneLengde + pauseLengde)) + toneLengde);
-        tone = toner(start:slutt);
-        for t=1:length(frekvenser)
-            filter1 = filter(filter2(t,1:bredde),1,tone);
-            if (max(filter1) > 0.75)
-                resultat = [resultat t];
+    for s=0:antallTall-1 %Går gjennom alle tallene som er trykket
+        resultat = []; %Tom matrise som vil inneholde frekvensene som er gått gjennom filteret
+        start = round((s*(toneLengde + pauseLengde)) + 1); %Når de ulike tonene starter
+        slutt = round((s*(toneLengde + pauseLengde)) + toneLengde); %Når de ulike tonene slutter
+        tone = toner(start:slutt); %Fjerner alt annet enn kun den aktuelle tonen
+        for t=1:length(frekvenser) %Går gjennom alle frekvensene i filteret
+            filter1 = filter(filter2(t,1:bredde),1,tone); %Kjører hver tone gjennom filteret og spytter ut hvor godt det passer
+            if (max(filter1) > 0.75) %Passer tonen mer en 75%...
+                resultat = [resultat t]; %...legges den til i resultatlisten
             end
         end
-        frekvens1 = frekvenser(resultat(1));
+        frekvens1 = frekvenser(resultat(1)); %Hver tone består av 2 frekvenser, der de ligger i resultat(1) og resultat(2) 
         frekvens2 = frekvenser(resultat(2));
-        sum = frekvens1 + frekvens2;
-        tallene = [tallene frekvensTilTall(sum)];
+        sum = frekvens1 + frekvens2; %Summene av frekvensene brukes til å finne hvilke tall de stammer fra
+        tallene = [tallene frekvensTilTall(sum)]; %Liste med hvilke tall som er trykket. Resultatet av frekvensTilTall legges inn
     end 
 end      
 
@@ -117,11 +117,11 @@ function toner = lag_og_spill_toner(tall)
     % Spiller av tonene med riktig frekvens
     sound(toner, Fs);
     %Finner nummeret utifra lyden
-    k=['Nummeret var: ' finn_tall(toner)];
+    k=['Nummeret var: ' finn_tall(toner)]; %Finner og printer hvilke tall som er trykket utifra matrisen med toner. 
     disp(k);
 end
 
-function tall = frekvensTilTall(sum)
+function tall = frekvensTilTall(sum) %Funksjon som finner hvilken sum av 2 frekvenser som hører til hvilket tastetrykk
     switch(sum)
         case(697+1209)
             tall = '1';
@@ -147,7 +147,7 @@ function tall = frekvensTilTall(sum)
             tall = '0';
         case(941+1477)
             tall = '#';
-        otherwise
+        otherwise %Hvis summen av frekvenser ikke passer med noen av de gitte frekvensene
             tall = 'FEIL!';
             
     end
